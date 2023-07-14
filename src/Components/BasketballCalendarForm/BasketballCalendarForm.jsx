745eca04e9bf40f2de0f8df09e6280bb0ddce5d8
Zsolt Nagy <info@zsoltnagy.eu>
2023-07-15 01:57:18 +0200

    form markup

--- src/Components/BasketballCalendarForm/BasketballCalendarForm.jsx
+++ src/Components/BasketballCalendarForm/BasketballCalendarForm.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import './BasketballCalendarForm.css';
 
 export default function BasketballCalendarForm() {
   return (
-    <div>BasketballCalendarForm</div>
+    <form>
+        <div className="form-row">
+            <label htmlFor="date">Date</label>
+            <input type="date" id="date" required />
+        </div>
+        <div className="form-row">
+            <label htmlFor="location">Location</label>
+            <input type="text" id="location" required />
+        </div>
+        <div className="form-row">
+            <label htmlFor="home">Home</label>
+            <input type="text" id="home" required />
+        </div>
+        <div className="form-row">
+            <label htmlFor="guest">Guest</label>
+            <input type="text" id="guest" required />
+        </div>
+        <div className="form-row">
+            <button type="submit">Add</button>
+        </div>
+    </form>
   );
 }
